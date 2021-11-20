c6cda89ba10cdb728b8fd6b9153da347df726d53
code-MIM <2153rud@gmail.com>
2021-11-21 01:36:54 +0900

    Youtube clone A-Z

--- src/server.js
+++ src/server.js
@@ -4,6 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = function(req, res) {
+    return res.send("<h1>hello world [form html]</h1>");
+}
+
+const handleLogin = function(req, res) {
+    return res.send({message:"Login here!"});
+}
+
+app.get("/", handleHome);
+app.get("/login", handleLogin)
+
 const handleListening = function() {
     console.log(`Server is listening on port http://localhost:${PORT}`);
 }
