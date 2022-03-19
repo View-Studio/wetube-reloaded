550e23b88929bf6a3c98d7d3992d76b4c15e4ac2
code-MIM <2153rud@gmail.com>
2022-03-19 23:23:52 +0900

    weTube #3.7 ~ #3.10 : 셋업, 서버, 컨트롤러, 미들웨어 복습.

--- src/server.js
+++ src/server.js
@@ -4,32 +4,26 @@ const PORT = 4000;
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
+const methodLogger = (req, res, next) => {
+  console.log(`METHOD: ${req.method}`);
   next();
 };
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  } else {
-    console.log("Allowed, you may continue.");
-    next();
-  }
+const routerLogger = (req, res, next) => {
+  console.log(`PATH: ${req.path}`);
+  next();
 };
 
-const handleHome = (req, res) => {
-  return res.send("<h1>I still love you.</h1>");
+const home = (req, res) => {
+  return res.send("<h1>Hello world.</h1>");
 };
-const handleProtected = (req, res) => {
-  return res.send("<h1>Welcome to the private lounge.</h1>");
+const login = (req, res) => {
+  return res.send("Login here!");
 };
 
-app.use(logger);
-app.use(privateMiddleware);
+app.use(methodLogger, routerLogger);
 
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
