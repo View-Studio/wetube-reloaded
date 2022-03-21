13f870853af05105ff4a7b6660bb09b1e51f2676
code-MIM <2153rud@gmail.com>
2022-03-22 00:39:18 +0900

    weTube #4.2: export와 import로 코드 정리하기.

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
-import res from "express/lib/response";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
@@ -8,24 +10,6 @@ const app = express();
 const logger = morgan("dev");
 
 app.use(logger);
-
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
